7706efa01bcbf1ec3e916439854583562fe40a19
Ngoc Quy <ngocquy020196@gmail.com>
2020-06-04 15:15:05 +0700

    [FIX] fix

--- src/interfaces/response.interface.ts
+++ src/interfaces/response.interface.ts
@@ -6,12 +6,12 @@ export interface IResponse {
 }
 
 export interface IResponseExpress extends Response {
-  ok(data: object): object;
-  created(data: object): object;
-  noContent(): object;
-  badRequest(message: string): object;
-  unauthorized(message: string): object;
-  forbidden(message: string): object;
-  notFound(message: string): object;
-  internalServerError(message: string): object;
+  ok(data: object): any;
+  created(data: object): any;
+  noContent(): any;
+  badRequest(message: string): any;
+  unauthorized(message: string): any;
+  forbidden(message: string): any;
+  notFound(message: string): any;
+  internalServerError(message: string): any;
 }
