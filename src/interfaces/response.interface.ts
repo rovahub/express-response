2e3615eaeaf8a10426945af0511fb2be57fb56fc
Ngoc Quy <ngocquy020196@gmail.com>
2020-10-14 11:43:47 +0700

    fix

--- src/interfaces/response.interface.ts
+++ src/interfaces/response.interface.ts
@@ -1,4 +1,4 @@
-import { Response } from 'express';
+import { Response } from "express";
 
 export interface IResponse {
   success(statusCode: number, data: object): object;
@@ -6,12 +6,12 @@ export interface IResponse {
 }
 
 export interface IResponseExpress extends Response {
-  ok(data: object, status: number): any;
-  created(data: object, status: number): any;
-  noContent(status: number): any;
-  badRequest(message: string, status: number): any;
-  unauthorized(message: string, status: number): any;
-  forbidden(message: string, status: number): any;
-  notFound(message: string, status: number): any;
-  internalServerError(message: string, status: number): any;
+  ok(data: object, status?: number): any;
+  created(data: object, status?: number): any;
+  noContent(status?: number): any;
+  badRequest(message: string, status?: number): any;
+  unauthorized(message: string, status?: number): any;
+  forbidden(message: string, status?: number): any;
+  notFound(message: string, status?: number): any;
+  internalServerError(message: string, status?: number): any;
 }
